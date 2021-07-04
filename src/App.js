34f12ffd214eac6cf1b85323507fac2d9b026b11
KoujiY <wegs8405@gmail.com>
2021-07-04 12:37:54 +0800

    0704_web_html_done
    WEB HTML切版畫面完成
    接著執行各頁面渲染與功能開發

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import CoffeeIntroduceGlobal from './pages/coffee-introduce/coffee-introduce-global'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <>
+        {/* LINK連結區塊，開發用 */}
+        <Link to="/coffee-introduce/coffee-introduce-global">
+          coffee-introduce-global
+        </Link>
+        <hr />
+        {/* 路由表 */}
+        <Switch>
+          <Route path="/coffee-introduce/coffee-introduce-global">
+            <CoffeeIntroduceGlobal />
+          </Route>
+        </Switch>
+      </>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/coffee-introduce/coffee-introduce-global.js
@@ -0,0 +1,9 @@
+function CoffeeIntroduceGlobal() {
+  return (
+    <>
+      <div>TEST</div>
+    </>
+  )
+}
+
+export default CoffeeIntroduceGlobal
